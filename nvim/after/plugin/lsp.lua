e47c9476bb8d1f195b0c6763fd6ea812c02fd904
Nils Gehlin <nils.gehlin@gmail.com>
2023-02-15 11:33:58 +0100

    remove spectral lsp, cant install

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -59,7 +59,7 @@ require('mason').setup()
 
 -- Enable the following language servers
 -- Feel free to add/remove any LSPs that you want here. They will automatically be installed
-local servers = { 'clangd', 'pyright', 'eslint', 'tsserver', 'jsonls', 'bashls', 'cssls', 'dockerls', 'graphql', 'html', 'spectral', 'sqlls' }
+local servers = { 'clangd', 'pyright', 'eslint', 'tsserver', 'jsonls', 'bashls', 'cssls', 'dockerls', 'graphql', 'html', 'sqlls' }
 
 -- Ensure the servers above are installed
 require('mason-lspconfig').setup {
