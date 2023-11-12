793a3c4d3d129f406bd2f0bca32be367e639b2ec
Nils Gehlin <nils.gehlin@stadium.com>
2023-11-12 09:40:24 +0100

    moved treesitter config to treesitter file

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,14 +1,98 @@
 return {
-    {
-        -- Highlight, edit, and navigate code
-        'nvim-treesitter/nvim-treesitter',
-        opts = {
-            ensure_installed = { "lua", "c_sharp" }
+    -- Highlight, edit, and navigate code
+    'nvim-treesitter/nvim-treesitter',
+
+    ---@type TSConfig
+    ---@diagnostic disable-next-line: missing-fields
+    opts = {
+        -- Add languages to be installed here that you want installed for treesitter
+        ensure_installed = {
+            'lua',
+            'vimdoc',
+            'vim',
+            'bash',
+            'c_sharp',
         },
-        dependencies = {
-            'nvim-treesitter/nvim-treesitter-textobjects',
-            'nvim-treesitter/playground',
+
+        -- Autoinstall languages that are not installed. Defaults to false (but you can change for yourself!)
+        auto_install = true,
+
+        highlight = { enable = true },
+        indent = { enable = true },
+        incremental_selection = {
+            enable = true,
+            keymaps = {
+                init_selection = '<c-space>',
+                node_incremental = '<c-space>',
+                scope_incremental = '<c-s>',
+                node_decremental = '<M-space>',
+            },
         },
-        build = ':TSUpdate',
-    }
+        textobjects = {
+            select = {
+                enable = true,
+                lookahead = true, -- Automatically jump forward to textobj, similar to targets.vim
+                keymaps = {
+                    -- You can use the capture groups defined in textobjects.scm
+                    ['aa'] = '@parameter.outer',
+                    ['ia'] = '@parameter.inner',
+                    ['af'] = '@function.outer',
+                    ['if'] = '@function.inner',
+                    ['ac'] = '@class.outer',
+                    ['ic'] = '@class.inner',
+                },
+            },
+            move = {
+                enable = true,
+                set_jumps = true, -- whether to set jumps in the jumplist
+                goto_next_start = {
+                    [']m'] = '@function.outer',
+                    [']]'] = '@class.outer',
+                },
+                goto_next_end = {
+                    [']M'] = '@function.outer',
+                    [']['] = '@class.outer',
+                },
+                goto_previous_start = {
+                    ['[m'] = '@function.outer',
+                    ['[['] = '@class.outer',
+                },
+                goto_previous_end = {
+                    ['[M'] = '@function.outer',
+                    ['[]'] = '@class.outer',
+                },
+            },
+            swap = {
+                enable = true,
+                swap_next = {
+                    ['<leader>a'] = '@parameter.inner',
+                },
+                swap_previous = {
+                    ['<leader>A'] = '@parameter.inner',
+                },
+            },
+        },
+
+    },
+    ---@param opts TSConfig
+    config = function(_, opts)
+        if type(opts.ensure_installed) == "table" then
+            ---@type table<string, boolean>
+            local added = {}
+            opts.ensure_installed = vim.tbl_filter(function(lang)
+                if added[lang] then
+                    return false
+                end
+                added[lang] = true
+                return true
+            end, opts.ensure_installed)
+        end
+        require("nvim-treesitter.configs").setup(opts)
+    end,
+
+    dependencies = {
+        'nvim-treesitter/nvim-treesitter-textobjects',
+        'nvim-treesitter/playground',
+    },
+    build = ':TSUpdate',
 }
