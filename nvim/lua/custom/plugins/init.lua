fa6f91208ed7cadfe34734b87dbec1cee532b414
Nils Gehlin <nils.gehlin@stadium.com>
2023-11-13 10:00:37 +0100

    add trouble nvim plugin

--- nvim/lua/custom/plugins/init.lua
+++ nvim/lua/custom/plugins/init.lua
@@ -4,5 +4,10 @@ return {
     'tpope/vim-rhubarb',
 
     -- Useful plugin to show you pending keybinds.
+    {
+        "folke/trouble.nvim",
+        dependencies = { "nvim-tree/nvim-web-devicons" },
+        opts = {},
+    },
     { 'folke/which-key.nvim', opts = {} },
 }
