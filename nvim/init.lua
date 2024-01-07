0cfcedfd9660fabd379a43f0c1f1c63e87506c6b
Nils Gehlin <nils.gehlin@gmail.com>
2024-01-07 19:57:10 +0100

    starting lsp refactor now

--- nvim/init.lua
+++ nvim/init.lua
@@ -128,72 +128,31 @@ require('nige')
 --  This function gets run when an LSP connects to a particular buffer.
 local on_attach = function(server_name)
   return function(_, bufnr)
-    -- NOTE: Remember that lua is a real programming language, and as such it is possible
-    -- to define small helper and utility functions so you don't have to repeat yourself
-    -- many times.
-    --
-    -- In this case, we create a function that lets us more easily define mappings specific
-    -- for LSP related items. It sets the mode, buffer and description for us each time.
-    local nmap = function(keys, func, desc)
-      if desc then
-        desc = 'LSP: ' .. desc
-      end
-
-      vim.keymap.set('n', keys, func, { buffer = bufnr, desc = desc })
-    end
-
+    local format_cmd
     if (server_name == "fsautocomplete") then
-      nmap('<leader>f', "<cmd>silent !fantomas %<CR>", '[F]ormat')
+      format_cmd = "<cmd>silent !fantomas %<CR>"
     else
-      nmap('<leader>f', vim.lsp.buf.format, '[F]ormat')
+      format_cmd = vim.lsp.buf.format
     end
+    vim.keymap.set('n', '<leader>f', format_cmd)
 
-    nmap('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
-    nmap('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction')
-
-    nmap('gd', require('telescope.builtin').lsp_definitions, '[G]oto [D]efinition')
-    nmap('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
-    nmap('gI', require('telescope.builtin').lsp_implementations, '[G]oto [I]mplementation')
-    nmap('<leader>D', require('telescope.builtin').lsp_type_definitions, 'Type [D]efinition')
-    nmap('<leader>ds', require('telescope.builtin').lsp_document_symbols, '[D]ocument [S]ymbols')
-    nmap('<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
-
-    -- See `:help K` for why this keymap
-    nmap('K', vim.lsp.buf.hover, 'Hover Documentation')
-    nmap('<C-k>', vim.lsp.buf.signature_help, 'Signature Documentation')
-
-    -- Lesser used LSP functionality
-    nmap('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
-    nmap('<leader>wa', vim.lsp.buf.add_workspace_folder, '[W]orkspace [A]dd Folder')
-    nmap('<leader>wr', vim.lsp.buf.remove_workspace_folder, '[W]orkspace [R]emove Folder')
-    nmap('<leader>wl', function()
-      print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-    end, '[W]orkspace [L]ist Folders')
-
-    -- Create a command `:Format` local to the LSP buffer
-    vim.api.nvim_buf_create_user_command(bufnr, 'Format', function(_)
-      vim.lsp.buf.format()
-    end, { desc = 'Format current buffer with LSP' })
+    vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename)
+    vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action)
+    vim.keymap.set('n', 'gd', require('telescope.builtin').lsp_definitions)
+    vim.keymap.set('n', 'gr', require('telescope.builtin').lsp_references)
+    vim.keymap.set('n', 'gI', require('telescope.builtin').lsp_implementations)
+    vim.keymap.set('n', '<leader>ds', require('telescope.builtin').lsp_document_symbols)
+    vim.keymap.set('n', 'K', vim.lsp.buf.hover)
+    vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help)
   end
 end
 
--- mason-lspconfig requires that these setup functions are called in this order
--- before setting up the servers.
 require('mason').setup()
 require('mason-lspconfig').setup()
 
--- Enable the following language servers
---  Feel free to add/remove any LSPs that you want here. They will automatically be installed.
---
---  Add any additional override configuration in the following tables. They will be passed to
---  the `settings` field of the server config. You must look up that documentation yourself.
---
---  If you want to override the default filetypes that your language server will attach to you can
---  define the property 'filetypes' to the map in question.
 local servers = {
   tsserver = {},
   marksman = {},
-
   lua_ls = {
     Lua = {
       workspace = { checkThirdParty = false },
@@ -201,10 +160,6 @@ local servers = {
     },
   },
 }
-
--- nvim-cmp supports additional completion capabilities, so broadcast that to servers
--- local capabilities = vim.lsp.protocol.make_client_capabilities()
--- calocal pabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
 
 vim.cmd [[ autocmd BufNewFile,BufRead *.bicep set filetype=bicep ]]
 
@@ -224,9 +179,7 @@ require("roslyn").setup({
   capabilities = capabilities,
 })
 
--- Ensure the servers above are installed
 local mason_lspconfig = require 'mason-lspconfig'
-
 mason_lspconfig.setup {
   ensure_installed = vim.tbl_keys(servers),
 }
