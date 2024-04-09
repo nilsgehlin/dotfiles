a36c834e525df9e11f1869231dd5f9e5870531dc
Nils Gehlin <nils.gehlin@stadium.com>
2024-04-09 20:22:28 +0200

    navigation -> oil

--- nvim/lua/plugins/oil.lua
+++ nvim/lua/plugins/oil.lua
@@ -3,7 +3,6 @@ return {
         'stevearc/oil.nvim',
         opts = {},
         lazy = false,
-        -- Optional dependencies
         dependencies = { "nvim-tree/nvim-web-devicons" },
         keys = {
             { "-", "<CMD>Oil<CR>", desc = "Open parent directory" },
