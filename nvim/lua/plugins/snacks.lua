d0a47797797026dd77142c4fbb4b5b59e66e5dcc
Nils Gehlin <nils.gehlin@stadium.com>
2026-04-24 06:05:49 +0200

    update

--- nvim/lua/plugins/snacks.lua
+++ nvim/lua/plugins/snacks.lua
@@ -1,9 +1,21 @@
 return {
   {
     "folke/snacks.nvim",
----@type snacks.Config
+    ---@type snacks.Config
     opts = {
       explorer = { enabled = false },
+      picker = {
+        formatters = {
+          file = {
+            filename_first = true, -- display filename before the file path
+            --- * left: truncate the beginning of the path
+            --- * center: truncate the middle of the path
+            --- * right: truncate the end of the path
+            ---@type "left"|"center"|"right"
+            truncate = "right",
+          },
+        },
+      },
     },
     keys = {
       { "<leader>gs", false },
