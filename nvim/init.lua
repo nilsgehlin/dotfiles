dfaac1ee27ee3d46fb9c9b0d0b13ff7fa20fe4ff
Nils Gehlin <nils.gehlin@stadium.com>
2026-06-03 21:22:33 +0200

    update

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,2 +1,11 @@
+-- Enable module caching for faster require() calls
+vim.loader.enable()
+
+-- Disable unused built-in plugins for faster startup
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+vim.g.loaded_matchit = 1
+vim.g.loaded_man = 1
+
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
